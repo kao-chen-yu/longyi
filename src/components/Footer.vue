2d3f49c7a63229cdf131b7372f3c23c4dc1d0e03
ChenYu Kao <ae6013@gmail.com>
2020-12-22 17:38:47 +0800

    aaa

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,11 +8,10 @@
                 <img class="logo" src="./../assets/logo.png">
                 <div>
                     <div class="footer-text">
-                    <p>龍儀科技 Longyi .</p>
-                    <p>高雄市路竹區路科五路92號4樓A室.</p>
-                    <p>Tel. +886-7-6955698.</p>
-                    <p>Mobile. +886-968391792.</p>
-                    <p>Email. +886-968391792.</p>
+                    <p>瀧儀生醫科股份有限公司© 2020 ALL RIGHTS RESERVED</p>
+                    <p>82151高雄市路竹區北嶺里路科五路92號4樓B室｜</p>
+                    <p>Tel：07-695-5080｜Fax:07-695-5082 </p>
+                    <p>Email:OOXXOOX@gmail.com</p>
                     </div>
                   <div class="footer-icon">
                         <font-awesome-icon class="icon" :icon="['fab', 'facebook']" size="2x" />
@@ -39,7 +38,7 @@
    left: 0;
    bottom: 0;
    width: 100%;
-   height: 350px;
+   height: 200px;
    background-color: rgb(3, 91, 162);
    color: white;
    
@@ -47,19 +46,21 @@
 
 .footer .logo {
     margin-top : 30px;
+    float:left;
 
 }
 
 .footer .footer-text{
-  padding-top:10px;
+  padding-top:30px;
   width:50%;
+  margin-left:100px;
   margin-right:0px;
   float: left;
 }
 
 .footer .footer-icon{
-  padding-top:10px;
-  width:30%;
+  padding-top:30px;
+  width:15%;
   margin-right:0px;
   float: right;
 }
